035ffd88021c7a47644d0a87a8c8abb166d79672
Daniel Kibby <daniel.kibby@live.co.uk>
2020-05-02 08:50:28 -0500

    Latest Updates

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
-
-function RenderDirectoryComponent({campsite}){
-    return(
-        <Card>        
-            <CardImg width="100%" src={campsite.image} alt={campsite.name} />
-            <CardImgOverlay>
-                <CardTitle>{campsite.name}</CardTitle>
-            </CardImgOverlay>
+function RenderDirectoryItem({campsite}) {
+    return (
+        <Card>
+            <Link to={`/directory/${campsite.id}`}>
+                <CardImg width="100%" src={campsite.image} alt={campsite.name} />
+                <CardImgOverlay>
+                    <CardTitle>{campsite.name}</CardTitle>
+                </CardImgOverlay>
+            </Link>
         </Card>
     );
 }
@@ -18,7 +20,7 @@ function Directory(props){
     const directory = props.campsites.map(campsite => {
     return (
         <div key={campsite.id} className="col-md-5 m-1">
-           <RenderDirectoryComponent campsite={campsite} />               
+           <RenderDirectoryItem campsite={campsite} />               
         </div>
     
         );
@@ -27,10 +29,20 @@ function Directory(props){
     return (
         <div className="container">
             <div className="row">
+                <div className="col">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>Directory</BreadcrumbItem>
+                    </Breadcrumb>
+                    <h2>Directory</h2>
+                    <hr />
+                </div>
+            </div>
+            <div className="row">
                 {directory}
-            </div>                
+            </div>
         </div>
-        );
+    );
     }
     
 
